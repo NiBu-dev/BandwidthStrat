f27e08cedea07b9044a97f83487c5140f4b51f5c
sahas023 <sahas023@gmail.com>
2017-11-01 19:30:22 +0100

    added id container, positive result

--- Strats/tut_mymy.py
+++ Strats/tut_mymy.py
@@ -1,8 +1,26 @@
 # SATURN
 
-
 def init_bw():
-    return 100
+    return 15
 
 def decision(new, list_entry):
-    return [1,[1 for x in list_entry]]
+    idList = []
+    for index in xrange(len(list_entry)):
+        if list_entry[index][0] == 'bronze':
+            idList.append(0)
+        elif list_entry[index][0] == 'silver':
+            idList.append(1)
+        elif list_entry[index][0] == 'gold':
+            idList.append(2)
+    return [newM(new), [x for x in idList]]
+
+
+def newM(newmemb):                              # TODO: change this function such that it doesn't take in account the skipped entries
+    if newmemb == 'bronze':
+        return 0
+    elif newmemb == 'silver':
+        return 1
+    elif newmemb == 'gold':
+        return 2
+
+
